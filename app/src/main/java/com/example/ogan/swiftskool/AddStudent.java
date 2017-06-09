3be525834f5bf126ebf5236be728b131dd7f7c63
Ogan Belema <Oganbelema@gmail.com>
2017-06-09 10:22:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ogan/swiftskool/AddStudent.java
@@ -0,0 +1,79 @@
+package com.example.ogan.swiftskool;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AddStudent extends Fragment {
+
+
+    WebView webView;
+    public AddStudent() {
+        // Required empty public constructor
+    }
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message message) {
+            switch (message.what) {
+                case 1:{
+                    webViewGoBack();
+                }break;
+            }
+        }
+    };
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        getActivity().setTitle("Add Student");
+        View view = inflater.inflate(R.layout.fragment_add_student, container, false);
+        webView = (WebView) view.findViewById(R.id.registerStudent);
+        webView.setInitialScale(1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(false);
+
+        webView.loadUrl("https://bristow.azurewebsites.net/Account/RegisterStudent");
+        webView.setWebViewClient(new WebViewClient());
+        webView.setOnKeyListener(new View.OnKeyListener(){
+
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK
+                        && event.getAction() == MotionEvent.ACTION_UP
+                        && webView.canGoBack()) {
+                    handler.sendEmptyMessage(1);
+                    return true;
+                }
+
+                return false;
+            }
+
+        });
+
+
+        return view;
+    }
+
+    private void webViewGoBack(){
+        webView.goBack();
+    }
+
+}
